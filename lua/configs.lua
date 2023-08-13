0d26b4db6400fd7fec8d79e615f67d18bc923983
Taufik Nurhidayat <fiik346@gmail.com>
2023-08-13 07:11:59 +0700

    hide line start

--- lua/configs.lua
+++ lua/configs.lua
@@ -16,8 +16,8 @@ require('formatting')
 -- indent
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
+    show_current_context = false,
+    show_current_context_start = false,
 }
 
 require("toggleterm").setup()
